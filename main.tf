8bb3220f2d689841ff48a368672e453fdd297da8
Abdillah Sarifudin <abdillah@pocketmath.com>
2022-01-11 03:20:06 +0800

    modified s3 bucket policy for public read.

--- main.tf
+++ main.tf
@@ -1,26 +1,23 @@
 resource "aws_s3_bucket" "mariacorner-s3-bucket" {
   bucket = var.s3-bucket-name
   acl    = "public-read"
-
-  policy = <<EOF
-{
-     "id" : "MakePublic",
-   "version" : "2012-10-17",
-   "statement" : [
-      {
-         "action" : [
-             "s3:GetObject"
-          ],
-         "effect" : "Allow",
-         "resource" : "arn:aws:s3:::${var.s3-bucket-name}/*",
-         "principal" : "*"
-      }
-    ]
-  }
-EOF
+  policy = data.aws_iam_policy_document.public-read-bucket-policy.json
 
   website {
     index_document = "index.html"
     error_document = "error.html"
+  }
+}
+
+data "aws_iam_policy_document" "public-read-bucket-policy" {
+  statement {
+    sid       = "Allow Public Read"
+    actions   = ["s3:GetObject"]
+    resources = ["arn:aws:s3:::${var.s3-bucket-name}", ]
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
   }
 }
