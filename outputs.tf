6aa9558174e89b034d15681d540b964f4a60db9a
Abdillah Sarifudin <abdillah@pocketmath.com>
2022-01-11 13:27:28 +0800

    add s3 website endpoint & arn outputs.

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "s3_bucket_website_endpoint" {
+  value = module.s3-bucket-mariacorner-website.s3_bucket_website_endpoint
+}
+
+output "s3_bucket_arn" {
+  value = module.s3-bucket-mariacorner-website.s3_bucket_arn
+}
