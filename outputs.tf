82cc1b2615ead9c27407344e7e3500b8d334136a
Abdillah Sarifudin <abdillah@pocketmath.com>
2022-01-12 13:26:49 +0800

    add s3 and cloudfront outputs.

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,19 @@ output "s3_bucket_website_endpoint" {
 output "s3_bucket_arn" {
   value = module.s3-bucket-mariacorner-website.s3_bucket_arn
 }
+
+output "s3_bucket_regional_domain_name" {
+  value = module.s3-bucket-mariacorner-website.s3_bucket_bucket_regional_domain_name
+}
+
+output "cloudfront_distribution_domain_name" {
+  value = aws_cloudfront_distribution.mariascorner-website-distribution.domain_name
+}
+
+output "cloudfront_distribution_hosted_zone_id" {
+  value = aws_cloudfront_distribution.mariascorner-website-distribution.hosted_zone_id
+}
+
+output "cloudfront_origin_access_identity_iam_arn" {
+  value = aws_cloudfront_origin_access_identity.mariascorner-website-oai.iam_arn
+}
