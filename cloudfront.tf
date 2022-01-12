a0a5b6b068a2a271ff641429251b428d025c5544
Abdillah Sarifudin <abdillah@pocketmath.com>
2022-01-12 13:26:30 +0800

    create cloudfront resources.

--- cloudfront.tf
+++ cloudfront.tf
@@ -1,12 +1,22 @@
 locals {
-  s3_origin_id = var.mariascorner-s3-bucket-name
+  s3_origin_id = module.s3-bucket-mariacorner-website.s3_bucket_bucket_regional_domain_name
 }
 
-module "mariascorner-website-cloudfront" {
-  source  = "terraform-aws-modules/cloudfront/aws"
-  version = "2.9.1"
+resource "aws_cloudfront_origin_access_identity" "mariascorner-website-oai" {
+  comment = "OAI for the Maria's Corner Marketing Website"
+}
 
-  aliases             = ["www.${data.aws_route53_zone.mariascorner-public-hosted-zone.name}"]
+resource "aws_cloudfront_distribution" "mariascorner-website-distribution" {
+  origin {
+    domain_name = module.s3-bucket-mariacorner-website.s3_bucket_bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.mariascorner-website-oai.cloudfront_access_identity_path
+    }
+  }
+
+  aliases             = ["www.${data.aws_route53_zone.mariascorner-public-hosted-zone.name}", "${data.aws_route53_zone.mariascorner-public-hosted-zone.name}"]
   enabled             = true
   is_ipv6_enabled     = true
   price_class         = "PriceClass_All"
@@ -14,47 +24,33 @@ module "mariascorner-website-cloudfront" {
   wait_for_deployment = false
   default_root_object = "index.html"
 
-  create_origin_access_identity = true
-  origin_access_identities = {
-    "${var.mariascorner-s3-bucket-name}-oai" = "OAI for the Maria's Corner Marketing Website"
-  }
-
-  origin = {
-    (local.s3_origin_id) = {
-      domain_name = module.s3-bucket-mariacorner-website.s3_bucket_bucket_regional_domain_name
-      s3_origin_config = {
-        origin_access_identity = "${var.mariascorner-s3-bucket-name}-oai"
-      }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
     }
   }
 
-  custom_error_response = [
-    {
-      "error_caching_min_ttl" = 300,
-      "error_code"            = 404
-      "response_code"         = 404,
-      "response_page_path"    = "/error.html"
-
-    },
-    {
-      "error_caching_min_ttl" = 300,
-      "error_code"            = 403
-      "response_code"         = 403,
-      "response_page_path"    = "/error.html"
-    }
-  ]
-
-  default_cache_behavior = {
+  default_cache_behavior {
     target_origin_id       = local.s3_origin_id
     viewer_protocol_policy = "redirect-to-https"
 
     allowed_methods = ["GET", "HEAD", "OPTIONS"]
     cached_methods  = ["GET", "HEAD"]
     compress        = true
-    query_string    = true
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+    min_ttl     = 0
+    default_ttl = 3600
+    max_ttl     = 86400
   }
 
-  viewer_certificate = {
+  viewer_certificate {
     acm_certificate_arn = data.aws_acm_certificate.mariascorner-issued-ssl-certificate-us-east-1.arn
     ssl_support_method  = "sni-only"
   }
