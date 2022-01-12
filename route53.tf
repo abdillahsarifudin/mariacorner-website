67ee66b6ca6a711a111a96c3f8dbfae2d0e0a49d
Abdillah Sarifudin <abdillah@pocketmath.com>
2022-01-12 13:27:23 +0800

    create records for main and sub domains.

--- route53.tf
+++ route53.tf
@@ -2,3 +2,25 @@ data "aws_route53_zone" "mariascorner-public-hosted-zone" {
   name         = var.mariascorner-domain-name
   private_zone = false
 }
+
+resource "aws_route53_record" "mariascorner-cloudfront-dns-record" {
+  zone_id = data.aws_route53_zone.mariascorner-public-hosted-zone.zone_id
+  name    = "mariascorner.co"
+  type    = "A"
+  alias {
+    name                   = aws_cloudfront_distribution.mariascorner-website-distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.mariascorner-website-distribution.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "mariascorner-subdomain-cloudfront-dns-record" {
+  zone_id = data.aws_route53_zone.mariascorner-public-hosted-zone.zone_id
+  name    = "www.mariascorner.co"
+  type    = "A"
+  alias {
+    name                   = aws_cloudfront_distribution.mariascorner-website-distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.mariascorner-website-distribution.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
